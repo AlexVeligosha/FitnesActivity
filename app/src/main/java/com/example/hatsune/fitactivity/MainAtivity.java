b83e0940443d26024fc60cbbaa09734005011d5f
AlexVeligosha <rpyiiika@gmail.com>
2016-12-13 15:23:01 +0200

    create MainActivity

---
+++ app/src/main/java/com/example/hatsune/fitactivity/MainAtivity.java
@@ -0,0 +1,17 @@
+package com.example.hatsune.fitactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hatsune on 13.12.16.
+ */
+
+public class MainAtivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
