3946c0840401086406acdedc39ff100a9e02d6fa
AlexVeligosha <rpyiiika@gmail.com>
2016-12-14 19:22:15 +0200

    added ActionBarDrawerToggle 14.12.16

--- app/src/main/java/com/example/hatsune/fitactivity/MainAtivity.java
+++ app/src/main/java/com/example/hatsune/fitactivity/MainAtivity.java
@@ -1,6 +1,11 @@
 package com.example.hatsune.fitactivity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -8,21 +13,24 @@ import android.view.MenuItem;
 
 public class MainAtivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
         
         intitToolbar();
+        initNavigationView();
     }
 
     private void intitToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
-        toolbar.setTitleTextColor(getResources().getColor(R.color.colorWhite));
+        //toolbar.setTitleTextColor(getResources().getColor(R.color.colorWhite));
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
@@ -31,6 +39,28 @@ public class MainAtivity extends AppCompatActivity {
         });
 
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.view_navigation_open, R.string.view_navigation_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawers();
+                switch (item.getItemId()){
+                    case R.id.actionHistoryItem:
+                        // Implement perfomance of active HistoryActivity
+                }
+
+                return true;
+            }
+        });
     }
 }
 
